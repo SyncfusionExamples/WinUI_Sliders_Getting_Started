722c3bf88a5747031150c82a9d1a8fada44f7449
vigneshramesh <vignesh.ramesh@syncfusion.com>
2021-04-13 21:02:11 +0530

    Desktop sample included.

---
+++ SliderGettingStartedUWP/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SliderGettingStartedUWP
+{
+    using Microsoft.UI.Xaml.Controls;
+    using Syncfusion.UI.Xaml.Sliders;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //SfSlider sfSlider = new SfSlider();
+            //sfSlider.Value = 50;
+            //sfSlider.ShowTicks = true;
+            //sfSlider.ShowLabels = true;
+            //sfSlider.ShowDivisors = true;
+            //sfSlider.DivisorHeight = 4;
+            //sfSlider.DivisorWidth = 4;
+            //sfSlider.ActiveTrackHeight = 4;
+            //sfSlider.InactiveTrackHeight = 4;
+            //this.Content = sfSlider;
+        }
+    }
+}
